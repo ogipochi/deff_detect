0372477bbf3817c956b87ea7431fcefc24f16132
ogihara666 <ogihara@coly.info>
2019-03-28 08:42:11 +0900

    add paste function

--- dev/react_app/src/front/src/config.js
+++ dev/react_app/src/front/src/config.js
@@ -1,3 +1,3 @@
-export const rootUrl = "http://localhost"
-//export const rootUrl = "http://35.243.93.197";
+//export const rootUrl = "http://localhost"
+export const rootUrl = "http://35.243.93.197";
 export const baseUrl = rootUrl + "/api";
